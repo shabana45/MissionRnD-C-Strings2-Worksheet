b41721719252a9476d5beb0398dbdad329aa52a0
shabana45 <shabana45@missionrnd.com>
2016-03-22 05:37:33 +0530

    Submit Commit

--- src/Consonants_Vowels.cpp
+++ src/Consonants_Vowels.cpp
@@ -18,9 +18,41 @@ NOTES: Don't create new string , Dont return anything ,you have been given two p
 */
 
 #include <stddef.h>
+int is_vowel(char* str)
+{
+	int count = 0;
+	for (int i = 0; str[i] != '\0'; i++)
+	if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' || str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U')
+		count++;
+	return count;
+}
+int no_splChar(char *str)
+{
+	int cnt = 0;
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		if (!((str[i] >= 'A'&&str[i] <= 'Z') || (str[i] >= 'a'&&str[i] <= 'z')))
+			cnt++;
+	}
+	return cnt;
+}
 
-
-void count_vowels_and_consonants(char *str,int *consonants, int *vowels){
-
-
+void count_vowels_and_consonants(char *str, int *consonants, int *vowels){
+	if (str == NULL)
+	{
+		*vowels = 0;
+		*consonants = 0;
+	}
+	else{
+		int len;
+		*vowels = 0;
+		*consonants = 0;
+		for (len = 0; str[len] != '\0'; len++);
+		if (is_vowel(str) > 0){
+			*vowels = is_vowel(str);
+			*consonants = len - is_vowel(str) - no_splChar(str);
+		}
+		else
+			*consonants = len;
+	}
 }
